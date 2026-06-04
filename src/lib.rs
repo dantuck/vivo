93bbcd79fcfb4ac87a5cd85438fc7826a01ea7f1
Daniel Tucker <code@dantuck.com>
2026-06-04 05:54:38 -0600

    feat: add update_s3_in_secrets, expose backup_remotes and description on Task

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,5 @@ pub mod update;
 pub mod config_editor;
 
 pub use crate::config::{build_cli, config_path_from, secrets_path_from, xdg_config_home, Secrets, VivoConfig};
-pub use crate::backup_config::{age_public_key, decrypt_sops_file, import_b2_credentials, parse_secrets, BackupConfig};
+pub use crate::backup_config::{age_public_key, decrypt_sops_file, import_b2_credentials, parse_secrets, update_s3_in_secrets, BackupConfig};
 pub use config_editor::{add_task, add_remote, remove_task, remove_remote, TaskSpec, RemoteSpec};
